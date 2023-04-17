05e939bcd978c7fd9bbe7248b9de12cc9866b7ee
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-04-17 23:51:53 +0530

    Modiefied ExpenseItem.js file

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,8 +1,8 @@
 import './ExpenseItems.css';
-function ExpenseItems() {
+function ExpenseItems(props) {
       const LocationOfExpenditure = ["FoodItem", "Petrol", "Movies"];
     return (<div className='Box'>
-        <div className='Title'><h2> Expense Items !</h2></div>
+        <div className='Title'><h2>{props.title }</h2></div>
         <div className='items'> <p>{LocationOfExpenditure[0]} for Rs 10 </p> </div>
         <div className='items'> <p>{LocationOfExpenditure[1]} for Rs 100</p></div>
         <div className='items'> <p>{LocationOfExpenditure[2]} Movies for Rs 200</p></div>
