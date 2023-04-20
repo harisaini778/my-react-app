4c7c3763e50b40c884cc0901ce142c9efc4d145d
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-04-21 03:14:23 +0530

    Task no 9 is working properly, corrected the code in App.js and ExpenseItems.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import './App.css';
 import ExpenseItems from './components/ExpenseItems';
 
 const App = ()=> {
-  let para = document.createElement("p");
-  para.textContent = "I am Hari learning React";
-  document.getElementById("root").append("para");
+  // let para = document.createElement("p");
+  // para.textContent = "I am Hari learning React";
+  // document.getElementById("root").append("para");
   let LocationOfExpenditure = [
     {
       title: "Food Item",
@@ -26,23 +25,9 @@ const App = ()=> {
     }
   ]
   return (
-    <div className="App">
-        <p> This is also visible ! </p>
-        <div><h2>This is Expense Items List !</h2></div>
-        {/* <ExpenseItems title={LocationOfExpenditure[0].title} amount={ LocationOfExpenditure[0].amount} date={LocationOfExpenditure[0].date}>
-        </ExpenseItems>
-        <ExpenseItems title={LocationOfExpenditure[1].title} amount={ LocationOfExpenditure[1].amount} date={LocationOfExpenditure[1].date}>
-        </ExpenseItems>
-        <ExpenseItems title={LocationOfExpenditure[2].title} amount={ LocationOfExpenditure[2].amount} date={LocationOfExpenditure[2].date}> */}
-      {/* </ExpenseItems> */}
-      {/* <ExpenseItems Location={LocationOfExpenditure.map((ele) => {
-        return (
-          ele.title,ele.amount,ele.date
-       ) 
-      })} > */}
-
-      {/* </ExpenseItems> */}
-      <ExpenseItems Location = {LocationOfExpenditure}></ExpenseItems>
+    <div className="App container text-bg-dark p-3 ">
+        <div className='font-monospace'><h2>This is Expense Items List !</h2></div>
+      <ExpenseItems locations={LocationOfExpenditure}></ExpenseItems>
     </div>
   );
 }
--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,25 +1,17 @@
-// import ExpenseDate from './ExpenseDate';
-import './ExpenseItems.css';
-const ExpenseItems = (Location)=> {
-    //   const LocationOfExpenditure = ["FoodItem", "Petrol", "Movies"];
-    const clickHandler = () => {
-        try {
-            console.log("delete button is clicked !!!");
-            const expenseItem = document.getElementById(Location.id);
-            expenseItem.parentNode.removeChild(expenseItem);
-        } catch(err){
-            console.log(err);   
-        }
-    }
-    console.log("This is thr location array"+Location[0].title);
-    return (<div className='Box' id={Location.id}>
-          {/* <ExpenseDate date={props.date}></ExpenseDate> */}
-        {/* <ExpenseItems title={props.title} amount={props.amount}>
-        </ExpenseItems> */}
-        <p>{Location.date}</p>
-        <p>{Location.amount}</p>
-        <p>{ Location.title }</p>
-        <button onClick={clickHandler}>Delete Expense</button>
-    </div>)
+//import './ExpenseItems.css';
+
+const ExpenseItems = (props) => {
+  return (
+    <div className='conatiner-fluid text-bg-light p-3"'>
+      {props.locations.map((location) => (
+        <div key={location.id} className='border border-dark'>
+          <p>Date: {location.date ? location.date.toLocaleDateString() : ''}</p>
+          <p>Title: {location.title}</p>
+          <p>Amount: {location.amount}</p> 
+        </div>
+      ))}
+    </div>
+  );
 }
 export default ExpenseItems;
+
