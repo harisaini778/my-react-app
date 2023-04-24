713a20cc960f4b3dc23a97c45d593a91782e9b17
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-04-25 00:21:00 +0530

    Added a Update Amount Button functionality

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,4 +1,4 @@
-//import './ExpenseItems.css';
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import CardElements from "./CardElements";
@@ -10,23 +10,56 @@ const ExpenseItems = (props) => {
     if (expenseElement) {
       expenseElement.remove();
     }
-  }
+  };
+
+  const [newAmounts, setNewAmounts] = useState({});
+
+  const UpdateExpenseHandler = (locationId) => {
+    const newAmount = "100$";
+    setNewAmounts((prevAmounts) => ({
+      ...prevAmounts,
+      [locationId]: newAmount,
+    }));
+  };
+
   return (
-       <CardElements>
+    <CardElements>
       {props.locations.map((location) => (
-        <div key={location.id} id={location.id} className='border border-dark card card-body bg-dark text-black'
-          style={{marginBottom:"5px"}}>
+        <div
+          key={location.id}
+          id={location.id}
+          className="border border-dark card card-body bg-dark text-black"
+          style={{ marginBottom: "5px" }}
+        >
           <ExpenseDate date={location.date}></ExpenseDate>
-              <div className="card card-body border border-dark"
-              style={{width:"50%",marginLeft:"250px"}}>
-            <ExpenseDetails details={[location.title, location.amount]}></ExpenseDetails>
-            <button className="btn btn-light font-monospace" onClick={deleteExpenseHandler} data-id={location.id}>
-             Delete Expense</button>
-            </div>
+          <div
+            className="card card-body border border-dark"
+            style={{ width: "50%", marginLeft: "250px" }}
+          >
+            <ExpenseDetails
+              details={[
+                location.title,
+                newAmounts[location.id] || location.amount,
+              ]}
+            ></ExpenseDetails>
+            <button
+              className="btn btn-light font-monospace"
+              onClick={deleteExpenseHandler}
+              data-id={location.id}
+            >
+              Delete Expense
+            </button>
+            <button
+              className="btn btn-success font-monopsace"
+              onClick={() => UpdateExpenseHandler(location.id)}
+            >
+              Update Amount
+            </button>
+          </div>
         </div>
       ))}
-      </CardElements>
+    </CardElements>
   );
-}
-export default ExpenseItems;
+};
 
+export default ExpenseItems;
